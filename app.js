85c2ff66f6ce6e4b68dde480e6dad78a61647c7f
IBK <adejumoibk702@gmail.com>
2024-01-19 05:17:19 +0100

    added cart size and color functionality

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const cartBtn = document.querySelector(".nav-item7");
 
 document.querySelector('.cart-background').style.display = "none"
 
-
 cartBtn.addEventListener("click",function name(params) {
 
   if (cartIndex == 0) {
@@ -18,7 +17,7 @@ cartBtn.addEventListener("click",function name(params) {
   } else {
     document.querySelector('.cart-background').style.display = "none"
     cartIndex = 0
-
+    
   }
   
 })
@@ -29,42 +28,38 @@ function renderProdcuts() {
   products.forEach((product) => {
     productsEl.innerHTML += `
     
-            <div class="item">
-                <div class="item-container">
-                    <div class="item-img">
-                        <img src="${product.imgSrc}" alt="${product.name}">
-                    </div>
-                    <div class="desc">
-                        <h5>${product.name}</h5>
-                        <h6><small>&#8358;</small>${product.price}</h6>
+    <div class="item">
+    <div class="item-container">
+    <div class="item-img">
+    <img src="${product.imgSrc}" alt="${product.name}">
+    </div>
+    <div class="desc">
+    <div class="add-to-wishlist">
+        <img src="images/stars (2).png" alt="add to wish list">
+    </div>
+    <h5>${product.name}</h5>
+                        <h6 class="mb-3"><small>&#8358;</small>${product.price}</h6>
                         
                         <form action="/submit" method="post">
-                        <label for="size">size:</label>
-                        <select id="size" name="size">
-                        <option value="medium">M</option>
+                        <label for="size-${product.id}">size:</label>
+                        <select id="size-${product.id}" name="size">
+                          <option value="medium">M</option>
                           <option value="small">S</option>
                           <option value="large">L</option>
                           <option value="extra-large">X-L</option>
-
                         </select>
-                        <br>
-                        <div class= "form-gap"></div>
-
-                    
-                      
-                    
-                        <label for="color">color:</label>
-                        <select id="color" name="color">
+                       
+                       <span class="left-margin"></span>
+                        
+                        <label for="color-${product.id}">color:</label>
+                        <select id="color-${product.id}" name="color">
                         <option value="black">Black</option>
-
                           <option value="red">Red</option>
                           <option value="blue">Blue</option>
                           <option value="green">Green</option>
                         </select>
-                    
+            
                         <br>
-                    
-                        
                       </form>
                       
                         <div class="add-to-cart button" onclick="addToCart(${product.id})">
@@ -83,21 +78,24 @@ function renderProdcuts() {
                     `;
   });
 }
-                    // <div class="add-to-wishlist">
-                    //     <img src="./icons/heart.png" alt="add to wish list">
-                    // </div>
 renderProdcuts();
 
 // cart array
 let cart = JSON.parse(localStorage.getItem("CART")) || [];
 updateCart();
 
-// ADD TO CART
-function addToCart(id) {
-  // check if prodcut already exist in cart
-  document.querySelector('.cart-background').style.display = "block"
+// console.log(selected);
 
-  if (cart.some((item) => item.id === id)) {
+
+// ADD TO CART
+
+function addToCart(id) {
+  document.querySelector('.cart-background').style.display = "block";
+  
+  const size = document.getElementById(`size-${id}`).value;
+  const color = document.getElementById(`color-${id}`).value;
+
+  if (cart.some((item) => item.id === id && item.size === size && item.color === color)) {
     changeNumberOfUnits("plus", id);
   } else {
     const item = products.find((product) => product.id === id);
@@ -105,12 +103,13 @@ function addToCart(id) {
     cart.push({
       ...item,
       numberOfUnits: 1,
+      size,
+      color,
     });
   }
 
   updateCart();
 }
-
 
 
 
@@ -139,21 +138,6 @@ document.querySelector('.total-price-nav').textContent = totalPrice.toFixed(2)
 
 }
 
-// render cart items
-// function renderCartItems() {
-//   cartItemsEl.innerHTML = ""; // clear cart element
-//   cart.forEach((item) => {
-//     cartItemsEl.innerHTML += `
-//         <div class="cart-item">
-//             <div class="item-info" onclick="removeItemFromCart(${item.id})">
-//                 <div class="item-name">${item.name}</div>
-//                 <img src="${item.imgSrc}" alt="${item.name}">
-//                 </div>
-//                 <!-- rest of the code remains unchanged -->
-//         </div>
-//       `;
-//     });
-//   }
 
 
 // render cart items
@@ -162,13 +146,13 @@ function renderCartItems() {
   cart.forEach((item) => {
     cartItemsEl.innerHTML += `
       <div class="cart-item">
-        <div class="item-info" onclick="removeItemFromCart(${item.id})">
+      <div class="item-info" onclick="removeItemFromCart(${item.id}, '${item.size}', '${item.color}')">
           <img src="${item.imgSrc}" alt="${item.name}">
         </div>
         <div class="unit-price">
           <small>&#8358;</small>${item.price}
-        </div>
-        <div class="units">
+          </div>
+          <div class="units">
           <div class="btn minus" onclick="changeNumberOfUnits('minus', ${item.id})">-</div>
           <div class="number">${item.numberOfUnits}</div>
           <div class="btn plus" onclick="changeNumberOfUnits('plus', ${item.id})">+</div>
@@ -184,10 +168,9 @@ function renderCartItems() {
 }
 
 // remove item from cart
-function removeItemFromCart(id) {
-  
-  cart = cart.filter((item) => item.id !== id);
 
+function removeItemFromCart(id, size, color) {
+  cart = cart.filter((item) => !(item.id === id && item.size === size && item.color === color));
   updateCart();
 }
 
@@ -212,4 +195,5 @@ function changeNumberOfUnits(action, id) {
 
   updateCart();
 }
+
 
--- products.js
+++ products.js
@@ -4,15 +4,13 @@ const products = [
     name: "Jungle Print Tee",
     price: 29.99,
     instock: 100,
-    imgSrc: "images/IMG_0213.png",
+    imgSrc: "images/jungle2.png",
   },
   {
     id: 1,
     name: "Alien Invasion",
     price: 24.99,
     instock: 43,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
     imgSrc: "images/IMG_0151.png",
   },
   {
@@ -20,8 +18,6 @@ const products = [
     name: "Made in IB",
     price: 19.99,
     instock: 10,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
       imgSrc: "images/made-in-ib.png",
     },
     {
@@ -29,26 +25,20 @@ const products = [
       name: "T-shirt 4",
       price: 25.99,
       instock: 5,
-      description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
-      imgSrc: "images/IMG_0151.png",
+     imgSrc: "images/IMG_0151.png",
   },
   {
     id: 4,
     name: "T-shirt 5",
     price: 29.99,
     instock: 4,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
-    imgSrc: "images/IMG_0213.png",
+    imgSrc: "images/jungle2.png",
   },
   {
     id: 5,
     name: "T-shirt 6",
     price: 39.99,
     instock: 40,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
     imgSrc: "images/made-in-ib.png",
   },
 ];
